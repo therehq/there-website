60aaf89c73f288dd10bf2b3ef950cef4946d1797
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-02-11 23:33:58 +0330

    feat: add firebase

---
+++ utils/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: 'there-192619.firebaseapp.com',
+  databaseURL: 'https://there-192619.firebaseio.com',
+  projectId: 'there-192619',
+  storageBucket: 'there-192619.appspot.com',
+  messagingSenderId: '122218890004',
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export default firebase
