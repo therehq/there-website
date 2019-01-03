dcde80a1f664e62c3e97a34becb72bea8e5d0925
Mo Rajabifard <mo.rajbi@gmail.com>
2019-01-03 17:09:07 +0330

    add assetprefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
+  assetPrefix: isProd? `https://there.netlify.com` : ``,
   exportPathMap: function() {
     return {
       '/': { page: '/' },
