753ea56342933f862adda0a77612e670f13c368e
Behnam Rajabifard <b.rajabifard@gmail.com>
2023-10-31 00:04:34 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-  assetPrefix: isProd? `https://there.netlify.com` : ``,
+  assetPrefix: isProd? `https://there.pm` : ``,
   exportPathMap: function() {
     return {
       '/': { page: '/' },
